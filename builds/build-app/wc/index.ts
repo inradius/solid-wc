d84552838a4fc653abaf7901b6d488060618eb50
Travis Stroud <travis@travisstroud.com>
2026-03-19 06:20:30 -0500

    feat: usilize @inradius/solid-wc-css-mixin

--- builds/build-app/wc/index.ts
+++ builds/build-app/wc/index.ts
@@ -1,8 +1,8 @@
 import App from '@app';
+import withCSS from '@inradius/solid-wc-css-mixin';
 import { compose, register } from 'component-register';
 import { withSolid } from 'solid-element';
 import { hostContractProps } from '@app/contexts';
 import css from '@dist/dev/index.css?inline';
-import withCSS from '@tools/with-css';
 
 compose(register('solid-wc', hostContractProps), withCSS(css), withSolid)(App);
